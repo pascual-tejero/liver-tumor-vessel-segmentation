44d1bddc35e53dd9bb4d03a026bf882d63e91415
Pascual Tejero Cervera <ge32fix@mytum.de>
2023-03-22 15:26:28 +0100

    Replace get_training_set.py

--- Liver and tumor/KiU-Net/KiU-Net-pytorch/data_prepare/get_training_set.py
+++ Liver and tumor/KiU-Net/KiU-Net-pytorch/data_prepare/get_training_set.py
@@ -47,7 +47,8 @@ for file in tqdm(os.listdir(para.train_ct_path)):
 
     # Downsampling and resampling of the CT data in cross-section, adjusting the spacing of the z-axis to 1mm for all data
     ct_array = ndimage.zoom(ct_array, (ct.GetSpacing()[-1] / para.slice_thickness, para.down_scale, para.down_scale), order=3)
-    seg_array = ndimage.zoom(seg_array, (ct.GetSpacing()[-1] / para.slice_thickness, 1, 1), order=0)
+    # seg_array = ndimage.zoom(seg_array, (ct.GetSpacing()[-1] / para.slice_thickness, 1, 1), order=0)
+    seg_array = ndimage.zoom(seg_array, (ct.GetSpacing()[-1] / para.slice_thickness, para.down_scale, para.down_scale), order=3)
 
     # Find the slice at the beginning and end of the liver region and expand the slice outwards in each direction
     z = np.any(seg_array, axis=(1, 2))
@@ -69,16 +70,16 @@ for file in tqdm(os.listdir(para.train_ct_path)):
 
     # Final saving of data as nii
     new_ct = sitk.GetImageFromArray(ct_array)
-
     new_ct.SetDirection(ct.GetDirection())
     new_ct.SetOrigin(ct.GetOrigin())
     new_ct.SetSpacing((ct.GetSpacing()[0] * int(1 / para.down_scale), ct.GetSpacing()[1] * int(1 / para.down_scale), para.slice_thickness))
 
     new_seg = sitk.GetImageFromArray(seg_array)
-
     new_seg.SetDirection(ct.GetDirection())
     new_seg.SetOrigin(ct.GetOrigin())
-    new_seg.SetSpacing((ct.GetSpacing()[0], ct.GetSpacing()[1], para.slice_thickness))
+    # new_seg.SetSpacing((ct.GetSpacing()[0], ct.GetSpacing()[1], para.slice_thickness))
+    new_seg.SetSpacing((ct.GetSpacing()[0] * int(1 / para.down_scale), ct.GetSpacing()[1] * int(1 / para.down_scale), para.slice_thickness))
 
     sitk.WriteImage(new_ct, os.path.join(new_ct_path, file))
-    sitk.WriteImage(new_seg, os.path.join(new_seg_dir, file.replace('volume', 'segmentation').replace('.nii', '.nii.gz')))
+    # sitk.WriteImage(new_seg, os.path.join(new_seg_dir, file.replace('volume', 'segmentation').replace('.nii', '.nii.gz')))
+    sitk.WriteImage(new_seg, os.path.join(new_seg_dir, file))
